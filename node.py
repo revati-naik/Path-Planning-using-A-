7370f24804f7630028bee6ce1aa46288ef0b9379
Satyarth Praveen <satyarth934@gmail.com>
2020-03-20 22:25:45 +0000

    Implemented to node class that stores every individual position and orientation of the robot including it's parent position.

--- node.py
+++ node.py
@@ -1,3 +1,6 @@
+import sys
+sys.dont_write_bytecode = True
+
 import utils
 
 class Node(object):
@@ -18,18 +21,54 @@ class Node(object):
 				 current_coords, 
 				 parent_coords, 
 				 orientation,
+				 parent_orientation,
 				 movement_cost, 
 				 goal_cost):
 		self.current_coords = current_coords
 		self.parent_coords = parent_coords
-		self.orientation = orientation
+		self.orientation = orientation % 360 if orientation is not None else None
+		self.parent_orientation = parent_orientation % 360 if parent_orientation is not None else None
 		self.movement_cost = movement_cost
-		self.goal_cost = goal_cost 
-		# if goal_cost != None:
-		# 	self.total_cost = movement_cost + goal_cost
-		# else:
-		# 	self.total_cost = goal_cost
-		# 	
+		self.goal_cost = goal_cost
+
+
+	def __gt__(self, other):
+		return ((self.movement_cost + self.goal_cost) > (other.movement_cost + other.goal_cost))
+
+
+	def __lt__(self, other):
+		return ((self.movement_cost + self.goal_cost) < (other.movement_cost + other.goal_cost))
+
+
+	def __ge__(self, other):
+		return ((self.movement_cost + self.goal_cost) >= (other.movement_cost + other.goal_cost))
+
+
+	def __le__(self, other):
+		return ((self.movement_cost + self.goal_cost) <= (other.movement_cost + other.goal_cost))
+
+
+	def __eq__(self, other):
+		return ((self.movement_cost + self.goal_cost) == (other.movement_cost + other.goal_cost))
+
+
+	def getXYCoords(self):
+		if self.current_coords is not None:
+			return (self.current_coords[1], self.current_coords[0])
+		return None
+
+
+	def getRowColCoords(self):
+		if self.current_coords is not None:
+			return (self.current_coords)
+		return None
+
+	
+	def getParentXYCoords(self):
+		if self.parent_coords is not None:
+			return (self.parent_coords[1], self.parent_coords[0])
+		return None
+
 
 	##
 	## Determines whether the specified random node is a duplicate of the current node.
@@ -42,9 +81,10 @@ class Node(object):
 	##
 	def isDuplicate(self, random_node):
 		if random_node.orientation is None:
-			return (utils.heuristic_cost(self.current_coords, random_node.current_coords) < 0.5)
+			return (utils.euclideanDistance(self.current_coords, random_node.current_coords) < 0.5)
 		
-		return ((utils.heuristic_cost(self.current_coords, random_node.current_coords) < 0.5) and (abs(self.orientation - random_node.orientation) < 30))
+		return ((utils.euclideanDistance(self.current_coords, random_node.current_coords) < 0.5) and (abs(self.orientation - random_node.orientation) < 30))
+
 
 	##
 	## Match all attributes of the current object with the random_node
@@ -58,6 +98,7 @@ class Node(object):
 	def deepMatch(self, random_node):
 		return (self.current_coords == random_node.current_coords and
 				self.parent_coords == random_node.parent_coords and
+				self.orientation == random_node.orientation and
 				self.movement_cost == random_node.movement_cost and
 				self.goal_cost == random_node.goal_cost)
 
@@ -81,5 +122,6 @@ class Node(object):
 	def printNode(self):
 		print "current_coords:\t", self.current_coords
 		print "parent_coords:\t", self.parent_coords
+		print "orientation:\t", self.orientation
 		print "movement_cost:\t", self.movement_cost
 		print "goal_cost:\t", self.goal_cost
