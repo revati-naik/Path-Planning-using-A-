c9b3b1ca300f2fa857dbbb07a89774767daa1011
Satyarth Praveen <satyarth934@gmail.com>
2020-03-21 02:19:53 +0000

    Writing code to take correct user input

--- Codes/main.py
+++ Codes/main.py
@@ -12,22 +12,26 @@ import actions
 import obstacles
 import node
 import utils
+import a_star
+import univ
 
 
 def main():
 
-	radius = int(input("Enter the robot radius: "))
-	clearance = int(input("Enter the clearance: "))
 
 	# User input for the start state
-	start_c, start_r = map(int, raw_input("Enter starting coordinates (x y): ").split())
-
+	start_c, start_r, start_orient = map(float, raw_input("Enter starting coordinates (x y) and orientation: ").split())
 	# User input for goal state
-	goal_c, goal_r = map(int, raw_input("Enter goal coordinates (x y): ").split())
+	goal_c, goal_r = map(float, raw_input("Enter goal coordinates (x y): ").split())
+	
+
+	radius = float(input("Enter the robot radius: "))
+	clearance = float(input("Enter the clearance: "))
+	step_size = float(input("Enter the robot step_size: "))
 
 	# write code to find the actual path using a star
 	start_time = time.clock()
-	path, viz_nodes = aStar(start_pos=(start_r,start_c), goal_pos=(goal_r,goal_c), robot_radius=radius, clearance=clearance, step_size=5, theta=30, duplicate_step_thresh=0.5, duplicate_orientation_thresh=30)
+	path, viz_nodes = a_star.aStar(start_pos=(start_r,start_c), goal_pos=(goal_r,goal_c), robot_radius=radius, clearance=clearance, step_size=step_size, theta=30, duplicate_step_thresh=0.5, duplicate_orientation_thresh=30)
 	print "Time to run A*:", time.clock() - start_time, "seconds"
 
 	# visualize path
